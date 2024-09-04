1c8a5c891bab11e926b1e141ee3682a21f0c6a62
Hepecho <echoeswyq@outlook.com>
2024-09-04 15:51:13 +0800

    update test code

--- vit-gpt2/src/main.py
+++ vit-gpt2/src/main.py
@@ -2,7 +2,10 @@
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 import torch
 from PIL import Image
+import os
+from os.path import join as ospj
 
+# 如果无法连接huggingfac，命令行添加 export HF_ENDPOINT="https://hf-mirror.com" 
 model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
@@ -15,10 +18,13 @@ model.to(device)
 max_length = 16
 num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-def predict_step(image_paths):
+image_dir = '../doc'
+
+def predict_step(image_dir):
+  image_names = os.listdir(image_dir)
   images = []
-  for image_path in image_paths:
-    i_image = Image.open(image_path)
+  for name in image_names:
+    i_image = Image.open(ospj(image_dir, name))
     if i_image.mode != "RGB":
       i_image = i_image.convert(mode="RGB")
 
@@ -31,9 +37,10 @@ def predict_step(image_paths):
 
   preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
   preds = [pred.strip() for pred in preds]
-  return preds
+  return image_names, preds
 
 
-tmp_preds = predict_step(['../doc/R-C.jpg', '../doc/fox.jpg']) # ['a woman in a hospital bed with a woman in a hospital bed']
+image_names, tmp_preds = predict_step(image_dir) # ['two people sitting on a park bench reading a book', 'a brown and white dog laying in the grass']
 
-print(tmp_preds)
+for name, caption in zip(image_names, tmp_preds):
+  print(f'image [{name}], caption [{caption}]')
