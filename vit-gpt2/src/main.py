99b2888d717f2305424e2087cfe316db0c0bd8bd
Hepecho <echoeswyq@outlook.com>
2024-09-05 16:30:55 +0800

    update test code

--- vit-gpt2/src/main.py
+++ vit-gpt2/src/main.py
@@ -40,7 +40,26 @@ def predict_step(image_dir):
   return image_names, preds
 
 
-image_names, tmp_preds = predict_step(image_dir) # ['two people sitting on a park bench reading a book', 'a brown and white dog laying in the grass']
+image_names, tmp_preds = predict_step(image_dir)
 
 for name, caption in zip(image_names, tmp_preds):
   print(f'image [{name}], caption [{caption}]')
+
+"""
+image [R-C.jpg], caption [two people sitting on a park bench reading a book]
+image [fox.jpg], caption [a brown and white dog laying in the grass]
+image [p1.jpg], caption [a woman standing in a field with flowers]
+image [e1.jpg], caption [a large body of water with a bridge over it]
+image [n1.jpg], caption [a man and woman dressed in black and white are hugging each other]
+image [c2.jpg], caption [a doll of a giraffe in a flower arrangement]
+image [c1.jpg], caption [a woman standing next to a statue of an elephant]
+image [n3.jpg], caption [a man and a woman standing on top of a surfboard]
+image [p2.jpg], caption [a woman is sleeping in a car with a blanket over her head]
+image [n2.jpg], caption [a man laying on the ground in front of a group of people]
+image [c4.jpg], caption [two men standing next to each other in a field]
+image [c3.jpg], caption [a woman holding an umbrella over her head]
+
+Ability: 单一object和常见action关系能准确描述
+Style: 量词 + 主体 + 动作/关系
+Limitation: 多个object无法统一或分别描述；非摄影作品的理解有偏差和错漏
+"""
