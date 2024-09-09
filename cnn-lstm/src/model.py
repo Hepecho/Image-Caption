c43d08ec41a35672652cfd7655f65b2a68f5ac09
Hepecho <echoeswyq@outlook.com>
2024-09-09 21:04:43 +0800

    complete EnocderCNN DecoderRNN class (#TODO sample code)

--- cnn-lstm/src/model.py
+++ cnn-lstm/src/model.py
@@ -7,22 +7,94 @@ from torch.nn.utils.rnn import pack_padded_sequence
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
         """Load the pretrained ResNet-152 and replace top fc layer."""
-        pass
+        super(EncoderCNN, self).__init__()
+        # resnet = models.resnet152(pretrained=True)
+        resnet = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.fc = nn.Linear(2048, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
     
     def forward(self, images):
         """Extract feature vectors from input images."""
-        pass
+        batch_size = images.shape[0]
+        with torch.no_grad():
+            features = self.resnet(images) # (B, 2048, 1, 1)
+        
+        features = features.reshape(batch_size, -1) # (B, 2048)
+        
+        features = self.fc(features)  # (B, embed_size)
 
-class DecoderRNN(nn.Moddule):
+        features = self.bn(features)
+
+        return features
+
+
+class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers, max_seq_length=20):
         """Set the hyper-parameters and build the layers."""
-        pass
+        super(DecoderRNN, self).__init__()
+        self.num_directions = 1 # 单向LSTM
+        self.hidden_size = hidden_size  # H
+        self.num_layers = num_layers  # L
+        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
+        self.lstm = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
 
     def forward(self, features, captions, lengths):
         """Decode image feature vectors and generates captions."""
-        pass
+        # features: torch tensor of shape# (B, embed_size).
+        # captions: torch tensor of shape (B, padded_length).
+        # lengths: list; valid length for each padded caption
+        embs = self.embedding(captions)  # (B, L, embed_size)
+        
+        inputs = torch.cat([features.unsqueeze(1), embs], dim=1)  # (B, 1 + L, embed_size)  把iamge附加到每个caption开头
+        
+        packed_inputs = pack_padded_sequence(inputs, lengths, batch_first=True)  # (real_L * B, embed_size)
+        
+        outputs, (_, _) = self.lstm(packed_inputs) # outputs.data (real_L * B, hidden_size)
+        
+        outputs = self.linear(outputs.data)  # (real_L * B, V)
+        return outputs
     
     def sample(self, features, states=None):
         """Generate captions for given image features using greedy search."""
         pass
         return sampled_ids
+
+if __name__ == '__main__':
+    import pickle
+    from build_vocab import Vocabulary
+    with open('data/vocab.pkl', 'rb') as f:
+        vocab = pickle.load(f)
+    vocab_size = len(vocab)
+    encoder = EncoderCNN(256).to('cuda')
+    decoder = DecoderRNN(256, 512, vocab_size, 1).to('cuda')
+
+    # data
+    from data_loader import get_loader
+    root = 'data/resized2014'
+    json = 'data/annotations/captions_train2014.json'
+    with open('data/vocab.pkl', 'rb') as f:
+        vocab = pickle.load(f)
+    import torchvision.transforms as transforms
+    transform = transforms.Compose([ 
+        transforms.RandomCrop(224),
+        transforms.RandomHorizontalFlip(), 
+        transforms.ToTensor(), 
+        transforms.Normalize((0.485, 0.456, 0.406), 
+                             (0.229, 0.224, 0.225))])
+    batch_size = 4
+    shuffle = True
+    num_workers = 1
+    data_loader = get_loader(root, json, vocab, transform, batch_size, shuffle, num_workers)
+    for batch in data_loader:
+        images, captions, lengths = batch
+        images = images.to('cuda')
+        captions = captions.to('cuda')
+        # lengths = torch.Tensor(lengths, dtype=torch.int64)
+        target = pack_padded_sequence(captions, lengths, batch_first=True).to('cuda')  # (real_L * B)
+        print(f'target shape: {target.data.shape}')
+        fearures = encoder(images)
+        outputs = decoder(fearures, captions, lengths)
+        exit()
